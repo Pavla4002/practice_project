4e56044925fade6cd052f79bf7e92683db806418
Павла <paha16kot@inbox.ru>
2022-12-26 10:53:58 +0300

    Еще адаптация и тщетные попытки что-то сделать

--- fun.js
+++ fun.js
@@ -2,8 +2,18 @@
 let blockBackG = document.querySelector('.box');
 
 blockBackG.addEventListener('click', function (e){
-    let element = e.target
-    if (element.classList.contains('value-q')){
-        element.classList.toggle('value-q-bg');
+    let element = e.target;
+    let rod = element.closest('.value-q');
+    // let count = 0
+    // if(element.closest('.block-question')){
+    //     count = element.closest('.block-question').getElementsByClassName('value-q-bg').length;
+    // }
+    if (element.closest('.value-q') && (element.classList.contains('click-on-me'))){
+        rod.classList.toggle('value-q-bg');
     }
+    // else if(element.closest('.value-q') && (element.classList.contains('click-on-me')) && count===1){
+    //
+    // }
+
+
 })
